7fb798f66a1a246d3f7dabc9c5c66e7f7f558a62
Vedant Roy <vroy101@gmail.com>
2020-06-28 03:28:41 -0400

    Updates

---
+++ precache-manifest.78c9aabb43278ed8a63273ed588ad22d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d9673c73416e3a37ff916c70663992",
+    "url": "/app.getvideodoc.com/index.html"
+  },
+  {
+    "revision": "e5a6f2751e03ea26635c",
+    "url": "/app.getvideodoc.com/static/css/main.dbeeb033.chunk.css"
+  },
+  {
+    "revision": "20645da676563ec67e62",
+    "url": "/app.getvideodoc.com/static/js/2.ba1f77cd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/app.getvideodoc.com/static/js/2.ba1f77cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5a6f2751e03ea26635c",
+    "url": "/app.getvideodoc.com/static/js/main.36d2bf16.chunk.js"
+  },
+  {
+    "revision": "75aa0c98471a124e5709",
+    "url": "/app.getvideodoc.com/static/js/runtime-main.47d72e25.js"
+  }
+]);
